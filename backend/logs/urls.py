03d333ab034d2df491cd1c674f4adc9ebecb272a
Friedrich Kriegsheim <friedrich.kriegsheim@rwth-aachen.de>
2021-11-07 16:43:03 +0100

    clean up old code framents

--- backend/logs/urls.py
+++ backend/logs/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [ 
     path('manage', views.ManageLogs.as_view()),
-    path('graph/<int:id>/', views.graph_example),
     path('logs', views.LogsJsonView.as_view()),
     path('select_comparisons',TemplateView.as_view(template_name='select_comparisons.html')),
     path('select_logs',views.SelectLogs.as_view()),
